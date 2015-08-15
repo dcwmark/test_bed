878e9e4d433a6787d650698523234541b20f1a50
Entaro Adun <dcwmark@gmail.com>
2015-08-15 00:08:27 -0400

    Added latest.

--- app/directives/filter-grid.js
+++ app/directives/filter-grid.js
@@ -2,18 +2,22 @@
  * test_bed/app/directives/filter-grid.js
 **/
 
-TB.app.directive('filterGrid', ['uiGridConstants',
-  function(uiGridConstants) {
-  'use strict';
-  
-  return {
-    restrict: 'E',
-    scope: {
-      gridData: '=gridData',
-      gridOptions: {
-        data: '=gridData'
-      }
-    },
-    templateUrl: 'directives/filter-grid.html'
-  };
-}]);
+TB.app.directive( 'filterGrid', ['uiGridConstants',
+    function(uiGridConstants) {
+      'use strict';
+      
+      return {
+        restrict: 'E',
+        scope: {
+          gridData: '=gridData'
+        },
+        templateUrl: 'directives/filter-grid.html',
+        link: function( scope, element, attr ) {
+          scope.gridOptions = {
+              data: 'xyzzy'
+          }
+        }
+      };
+    }
+] );
+
