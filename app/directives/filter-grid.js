862e36f3fd7144f81a9c02d5f34f2206a6b7006b
Entaro Adun <dcwmark@gmail.com>
2015-08-25 23:57:14 -0400

    Selection filter.

--- app/directives/filter-grid.js
+++ app/directives/filter-grid.js
@@ -14,8 +14,41 @@ TB.app.directive( 'filterGrid', ['uiGridConstants',
       templateUrl: 'directives/filter-grid.html',
       link: function(scope, element, attrs) {
         
+        scope.highLightFilterHeader = function(
+            row, rowRenderIndex, col, colRenderIndex) {
+          if ( col.filters[0].term ) {
+            return 'header-filtered';
+          } else {
+            return '';
+          }
+        };
+        
         scope.gridOptions = scope.gridOptions || {
-          enableFiltering: true
+          onRegisterApi: function(gridApi) {
+            scope.gridApi = gridApi;
+          },
+          enableFiltering: true,
+          columnDefs: [{
+            field: 'id',
+            width: '4%'
+          }, {
+            // default
+            field: 'name',
+            headerCellClass: scope.highLightFilterHeader
+          }, {
+            // pre-populated search field
+            field: 'gender',
+            filter: {
+              type: uiGridConstants.filter.SELECT,
+              selectOptions: [{
+                value: '1', label: 'male'
+              }, {
+                value: '2', label: 'female'
+              }]
+            },
+//            cellFilter: 'mapGender',
+            headerCellClass: scope.highLightFilterHeader
+          }]
         };
         
         scope.$watch('gridData', function(gridData) {
@@ -28,4 +61,18 @@ TB.app.directive( 'filterGrid', ['uiGridConstants',
       }
     };
   }
-] );
+] )
+.filter('mapGender', function() {
+  var genderHash = {
+    1: 'male',
+    2: 'female'
+  };
+  
+  return function(input) {
+    if (!input){
+      return '';
+    } else {
+      return genderHash[input];
+    }
+  };
+});
